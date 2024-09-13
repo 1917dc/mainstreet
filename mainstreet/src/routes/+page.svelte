686a1372b8d7be51fa07eb3565c2853f857f6b1d
Luiz Gustavo <142899763+1917dc@users.noreply.github.com>
2024-09-12 21:37:51 -0300

    Criando o projeto.

---
+++ mainstreet/src/routes/+page.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import logo from '$lib/assets/logo.png'
+	import {TableOfContents, tocCrawler, Accordion, AccordionItem} from "@skeletonlabs/skeleton";
+</script>
+<body class="scroll-smooth scroll-mt-[100px]"></body>
+<div class="text-center">
+	<div class="">
+		<h1 class="h1 m-3 select-none">BARBEARIA <br> <span class="text-primary-500">MAINSTREET</span></h1>
+
+	</div>
+	<div class="container text-center">
+		<h2 class="h2">Quem somos?</h2>
+		<div class="select-none">
+			<p class="m-5">
+				A <span class="text-primary-500">Barbearia Mainstreet</span> é mais do que um lugar para cortar o cabelo – é um espaço onde o estilo clássico encontra o conforto moderno. Seguimos a tradição <span class="text-secondary-400">old school</span> das barbearias, oferecendo cortes de cabelo e barba para <span class="text-secondary-500">adultos e crianças</span>, sempre com aquele toque especial que só um bom barbeiro pode proporcionar.
+			</p>
+			<p class="m-5">
+				Nossa equipe é composta por barbeiros experientes e apaixonados pelo que fazem. Cada um deles está à disposição para <span class="text-secondary-500">agendar seu horário</span> e garantir que você receba um atendimento personalizado e de qualidade.
+			</p>
+			<p class="m-5">
+				Além dos serviços de corte e barba, <span class="text-secondary-500">também oferecemos tratamentos de pele</span>, proporcionando uma experiência completa de autocuidado. E para tornar sua visita ainda mais agradável, <span class="text-secondary-500">oferecemos uma seleção de bebidas</span> para acompanhar o momento.
+			</p>
+			<p class="m-5">
+				Se quiser relaxar um pouco mais, nossa <span class="text-secondary-500">mesa de sinuca</span> está à sua disposição para aquele jogo descontraído entre amigos enquanto aguarda.
+			</p>
+			<p class="m-5">
+				Na <span class="text-primary-500">Barbearia Mainstreet</span>, cada detalhe é pensado para fazer você se sentir em casa.
+				Venha nos conhecer e vivenciar a verdadeira experiência de uma barbearia old school!
+			</p>
+
+		</div>
+	</div>
+</div>
